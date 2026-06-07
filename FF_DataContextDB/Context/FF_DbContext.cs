e2525bdc51ccfd5c58a2a45d1ec84d4349e6d325
Oscar Naranjo Zúñiga <Oscarnaranjoz13@gmail.com>
2026-06-07 11:23:42 -0600

    DB Connection
    Se realizo la migracion con EF, generacion de modelos y contexto

---
+++ FF_DataContextDB/Context/FF_DbContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using FF_ModelsDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FF_DataDB.Context;
+
+public partial class FF_DbContext : DbContext
+{
+    public FF_DbContext()
+    {
+    }
+
+    public FF_DbContext(DbContextOptions<FF_DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Source> Sources { get; set; }
+
+    public virtual DbSet<SourceItem> SourceItems { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Roles__3214EC0716AB39D5");
+
+            entity.HasIndex(e => e.Name, "UQ__Roles__737584F6814B4C40").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Source>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Sources__3214EC0782B5F4D9");
+
+            entity.Property(e => e.ComponentType).HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Name).HasMaxLength(200);
+            entity.Property(e => e.Url).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<SourceItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__SourceIt__3214EC075788BD3E");
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Source).WithMany(p => p.SourceItems)
+                .HasForeignKey(d => d.SourceId)
+                .HasConstraintName("FK_SourceItems_Sources");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07D2C32D74");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__536C85E4818DCC46").IsUnique();
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(25);
+            entity.Property(e => e.PasswordHash).HasMaxLength(500);
+            entity.Property(e => e.Username).HasMaxLength(200);
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.RoleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Users_Roles");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ FF_ModelsDB/Models/SourceItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FF_ModelsDB.Models;
+
+public partial class SourceItem
+{
+    public int Id { get; set; }
+
+    public int? SourceId { get; set; }
+
+    public string? Json { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Source? Source { get; set; }
+}
